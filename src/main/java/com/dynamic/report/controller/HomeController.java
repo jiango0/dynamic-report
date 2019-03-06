fcfceb99aa327083d0e50c2f2b0e8ff6d42b1e30
jiangzhichao <switchrouter@qq.com>
2019-03-06 17:49:42 +0800

    提交代码

---
+++ src/main/java/com/dynamic/report/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.dynamic.report.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
+public class HomeController {
+
+    @RequestMapping("")
+    public String homeIndex() {
+        return "main";
+    }
+
+}
